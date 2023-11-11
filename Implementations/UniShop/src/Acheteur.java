7c619868a5c3af89f26bd0810561bd170a103f5b
Avunz <56303117+Avunz@users.noreply.github.com>
2023-11-11 11:10:35 -0500

    initialisation d'implementation + inscription

---
+++ Implementations/UniShop/src/Acheteur.java
@@ -0,0 +1,14 @@
+public class Acheteur {
+
+    String nom, prenom, pseudo, email, adresse;
+    int telephone;
+
+    public Acheteur(String nom, String prenom, String pseudo, String email, String adresse, int telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.pseudo = pseudo;
+        this.email = email;
+        this.adresse = adresse;
+        this.telephone = telephone;
+    }
+}
