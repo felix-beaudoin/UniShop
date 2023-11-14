1924620611d0d91e8588831360057e36691c0802
Avunz <56303117+Avunz@users.noreply.github.com>
2023-11-14 10:51:42 -0500

    afficherproduit + rechercheproduit

--- Implementations/UniShop/src/Main.java
+++ Implementations/UniShop/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenue sur UniShop, votre plateforme d'achats et de reventes en ligne pour étudiants.");
@@ -5,7 +7,31 @@ public class Main {
         Authentification auth = new Authentification();
         Acheteur a = auth.accueil();
 
-        AfficherCatalogue afficherCatalogue = new AfficherCatalogue(a);
+        System.out.println("Que voulez-vous faire?");
+        System.out.println("1- Afficher le catalogue des produits");
+        System.out.println("2- Rechercher un produit particulier");
+        System.out.println("0- Quitter");
+
+        Scanner in = new Scanner(System.in);
+        String inp = in.nextLine();
+
+        boolean accueilLoop = true;
+        while (accueilLoop) {
+            try {
+                int n = Integer.parseInt(inp);
+
+                switch (n) {
+                    case 1: AfficherCatalogue afficherCatalogue = new AfficherCatalogue(a);
+                    case 2: RechercheProduits rechercheProduits = new RechercheProduits(a);
+                }
+
+            } catch (NumberFormatException nfe){
+                System.out.println("Choix non comprix, que voulez-vous faire?");
+            }
+        }
+
+
+
 
     }
 
--- Implementations/UniShop/src/Produit.java
+++ Implementations/UniShop/src/Produit.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+
 public class Produit {
     TypeProduit type;
     int prix, nbLikes; //prix en cents, pour éviter les nombres flottants
     String nom;
     String description;
     Vendeur vendeur;
+
+    HashMap<Acheteur, String[]> commentaires;
 
     public Produit(TypeProduit type, int prix, String nom, String description, Vendeur vendeur) {
         this.type = type;
