9aedbe82898b0158a4548914b2fc67c1df8c13fe
Meriem Ghaoui <mohamed.thameur.sassi@umontreal.ca>
2023-12-31 05:53:18 -0500

    creation billet

---
+++ Implementations/UniShop/src/Main/Billet.java
@@ -0,0 +1,59 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Billet {
+    Acheteur acheteur;
+    String  probleme;
+    String solution;
+    String code;
+    String description;
+
+    public Billet(Acheteur acheteur, String probleme, String solution, String code, String description) {
+        this.acheteur = acheteur;
+        this.probleme = probleme;
+        this.solution = solution;
+        this.code = code;
+        this.description = description;
+    }
+
+    public Acheteur getAcheteyr() {
+        return acheteur;
+    }
+
+    public void setAcheteyr(Acheteur acheteyr) {
+        this.acheteur = acheteyr;
+    }
+
+    public String getProbleme() {
+        return probleme;
+    }
+
+    public void setProbleme(String probleme) {
+        this.probleme = probleme;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getUID() {
+        return code;
+    }
+
+    public void setUID(String code) {
+        code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
