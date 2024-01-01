8bfde442e944c5fd1930542d12d3d7555ee30595
tglucas1 <121174416+tglucas1@users.noreply.github.com>
2023-12-31 19:31:47 -0500

    3 tests suiveurs

--- Implementations/UniShop/src/Main/AcheteurTest.java
+++ Implementations/UniShop/src/Main/AcheteurTest.java
@@ -59,4 +59,54 @@ public class AcheteurTest {
         String expectedOutput = "La liste des acheteurs est vide.\n";
         assertEquals(expectedOutput, outContent.toString());
     }
+    @Test
+    void gererSuiveurMultiple() {
+        //Arrange
+        Acheteur a = new Acheteur("TG","Lucas","LTG","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur b = new Acheteur("TG","Lucas","LTR","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur c = new Acheteur("TG","Lucas","LTT","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur d = new Acheteur("TG","Lucas","LTY","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur e = new Acheteur("TG","Lucas","LTU","lucas@hotmail.com","123 rue larue","514-123-1234");
+
+        //Act
+        a.UtilisateurSuivi.add(b.pseudo);
+        a.UtilisateurSuivi.add(c.pseudo);
+        a.UtilisateurSuivi.add(c.pseudo);
+        a.UtilisateurSuivi.add(c.pseudo);
+
+
+        //Arrange
+        int expectedOutput = 4;
+        assertEquals(expectedOutput,a.UtilisateurSuivi.size());
+
+    }
+    @Test
+    void gererSuiveurNotEmpty() {
+        //Arrange
+        Acheteur a = new Acheteur("TG","Lucas","LTG","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur b = new Acheteur("TG","Lucas","LTR","lucas@hotmail.com","123 rue larue","514-123-1234");
+
+        //Act
+        a.UtilisateurSuivi.add(b.pseudo);
+
+        //Arrange
+        String expectedOutput = "LTR";
+        assertEquals(expectedOutput,a.UtilisateurSuivi.get(0));
+
+    }
+    @Test
+    void gererSuiveurChangement() {
+        //Arrange
+        Acheteur a = new Acheteur("TG","Lucas","LTG","lucas@hotmail.com","123 rue larue","514-123-1234");
+        Acheteur b = new Acheteur("TG","Lucas","LTR","lucas@hotmail.com","123 rue larue","514-123-1234");
+
+        //Act
+        b.pseudo = "LTY";
+        a.UtilisateurSuivi.add(b.pseudo);
+
+        //Arrange
+        String expectedOutput = "LTY";
+        assertEquals(expectedOutput,a.UtilisateurSuivi.get(0));
+
+    }
 }
